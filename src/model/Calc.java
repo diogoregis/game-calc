e2366e4899fb293c9d0c9afb4b90a02629fe9b29
Diogo Regis <diogo.regis.silva@gmail.com>
2023-11-02 15:08:06 -0300

    criando atributos e get para a Calc

--- src/model/Calc.java
+++ src/model/Calc.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Calc {
+
+    private int dificuldade;
+    private int operacao, valor01, valor02, resultado;
+
+    public Calc(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public int getOperacao() {
+        return operacao;
+    }
+
+    public int getValor01() {
+        return valor01;
+    }
+
+    public int getValor02() {
+        return valor02;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
 }
